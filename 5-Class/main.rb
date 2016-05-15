e0b308307da069c7455eca41e4ac18b920759195
julialeonova92 <julia.leonova92@gmail.com>
2016-05-15 18:20:50 +0300

    Update main.rb

--- 5-Class/main.rb
+++ 5-Class/main.rb
@@ -1,30 +1,27 @@
-#main
-puts "The five longest movies: \n \n"
-puts new_hash.mov_hsh.sort_by{ |hsh| hsh.longe.to_i}.last(5).
-	map{|mov| [mov.title + ": " + mov.longe]}
+require 'csv'
+require './Movie'
+require './MovieList'
+
+mov_list=MovieList.new
+puts "All of comedy, sorted by release date: \n "
+puts mov_list.by_genre("Comedy")
 puts "\n"
 
-puts "All of comedy, sorted by release date: \n \n"
-puts new_hash.mov_hsh.find_all{ |hsh| hsh.genre.include?("Comedy")}.
-	sort_by {|hsh| hsh.date}.
-	map{|mov| [mov.title + ": " + mov.genre + ", " + mov.date]}
+puts "The five longest movies: \n "
+puts mov_list.by_longe
 puts "\n"
 
-puts "The list of the directors in alphabetical order (no repeats!) \n \n"
-puts new_hash.mov_hsh.sort_by{|hsh| hsh.director}.map{|dir| dir.director}.uniq
+puts "The list of the directors in alphabetical order (no repeats!): \n "
+puts mov_list.alphabet_dir
 puts "\n"
 
-non_usa=new_hash.mov_hsh.reject{|hsh| hsh.country==("USA")}.size
-puts "The number of movies produced not in the USA is: " + non_usa.to_s
+puts "The number of movies produced not in the follow country is: " + mov_list.non_country("USA")
 puts "\n"
 
-puts "The number of movies grouped by director \n \n"
-puts new_hash.mov_hsh.group_by{|hsh| hsh.director}.
-	map{|dir, group| ["Director is #{dir}" + ", namber of his movies: #{group.size.to_s}"]}
+puts "The number of movies grouped by director: \n "
+puts mov_list.group_dir
 puts "\n"
 
-puts "The number of movies produced each month \n \n"
-puts new_hash.mov_hsh.map{|hsh| Date._parse(hsh.date)}.
-	group_by{|k| k[:mon]}.
-	map{|mon, group| ["mon: #{mon.to_s}," + " number of movies: #{group.size.to_s}"]}
-
+puts "The number of movies produced each month: \n "
+puts mov_list.each_month
+puts "\n"
